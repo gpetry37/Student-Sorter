3d7787030f5be0223d0b512d7c42907e8369f475
gpetry37 <gpetry37@gmail.com>
2020-09-11 12:22:24 -0400

    Error support for invalid files added

--- src/main.rb
+++ src/main.rb
@@ -1,26 +1,29 @@
 #!/usr/bin/env ruby
 
 # require 'student.rb'
+require 'csv'
 
-# Reads in the csv file
-def read_file
+# Gets the input csv file
+def get_file
+  # Gets the file from the user
   puts "Enter csv file name to be read from 'data' folder:"
-  file = gets
+  file = gets.strip()
 
+  # Builds out filepath
   filepath = File.expand_path("..", Dir.pwd)
   filepath += "/data/#{file}"
 
+  # Error handling for input file
   valid_file = File.file?(filepath)
   file_check(valid_file)
 end
 
 # Checks whether or not a file is valid
 def file_check(valid_file)
-  until valid_file do
+  unless valid_file
     puts "Invalid file. Please try again!"
-    read_file
+    get_file
   end
 end
 
-read_file
-
+get_file
