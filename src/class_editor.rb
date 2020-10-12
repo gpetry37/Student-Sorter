899f73d9cfd73897cacf6c311dd9b75227bf02f9
gpetry37 <gpetry37@gmail.com>
2020-10-12 17:19:21 -0400

    Added loops and removed recursion in 2 files.

--- src/class_editor.rb
+++ src/class_editor.rb
@@ -5,76 +5,78 @@
 
 # Decision tree for deciding what information to modify, if any
 def q_modify(csv)
-  puts "Would you like to modify information in the current table? (y/n)"
-  edit = gets.strip
+  loop do
+    puts "Would you like to modify information in the current table? (y/n)"
+    edit = gets.strip
 
-  if edit == "Y" or edit == "y"
-    puts "Would you like to add a new student, remove a student or edit an existing student? (a/d/e)"
-    action = gets.strip
-    if action == "A" or action == "a"
-      add_csv(csv)
-    elsif action == "D" or action == "d"
-      del_csv(csv)
-    elsif action == "E" or action == "e"
-      edit_csv(csv)
+    if edit == "Y" or edit == "y"
+      puts "Would you like to add a new student, remove a student or edit an existing student? (a/d/e)"
+      action = gets.strip
+      if action == "A" or action == "a"
+        csv = add_csv(csv)
+      elsif action == "D" or action == "d"
+        csv = del_csv(csv)
+      elsif action == "E" or action == "e"
+        csv = edit_csv(csv)
+      else
+        puts "Not a valid response! Please try again."
+      end
+    elsif edit == "N" or edit == "n"
+      puts "Moving on to group creation."
+      return csv
     else
       puts "Not a valid response! Please try again."
-      q_modify(csv)
     end
-  elsif edit == "N" or edit == "n"
-    puts "Moving on to group creation."
-    return csv
-  else
-    puts "Not a valid response! Please try again."
-    q_modify(csv)
   end
 end
 
 # Decision Trees for adding students to CSV Table
 def add_csv(csv)
-  puts "Adding a new student!"
-  puts "Please enter the email of the student to be added."
-  add_email = gets.strip
-  puts "Please enter the first name of the student to be added."
-  add_fname = gets.strip
-  puts "Please enter the last name of the student to be added."
-  add_lname = gets.strip
-  puts "Please enter the class section of the student to be added."
-  add_section = gets.strip
-  puts "Please enter the first major of the student to be added."
-  add_major1 = gets.strip
-  puts "Please enter the second major of the student to be added. If none, hit enter."
-  add_major2 = gets.strip
-  if add_major2 == ""
-    add_major2 = nil
-  end
-  puts "Please enter the first minor of the student to be added. If none, hit enter."
-  add_minor1 = gets.strip
-  if add_minor1 == ""
-    add_minor1 = nil
-  end
-  puts "Please enter the second minor of the student to be added. If none, hit enter."
-  m4 = true
-  add_minor2 = gets.strip
-  if add_minor2 == ""
-    add_minor2 = nil
-  end
-
-  unless add_fname == "" or add_lname == "" or add_email == "" or add_section == "" or add_major1 == ""
-    student = Student.new(fname = add_fname, lname = add_lname, email = add_email, section = add_section, major1 = add_major1, major2 = add_major2, minor1 = add_minor1, minor2 = add_minor2)
-    if csv.has_key?(student.section)
-      array = csv[student.section]
-      array.push(student)
-      csv[student.section] = array
-    else
-      array = [student]
-      csv[student.section] = array
+  loop do
+    puts "Adding a new student!"
+    puts "Please enter the email of the student to be added."
+    add_email = gets.strip
+    puts "Please enter the first name of the student to be added."
+    add_fname = gets.strip
+    puts "Please enter the last name of the student to be added."
+    add_lname = gets.strip
+    puts "Please enter the class section of the student to be added."
+    add_section = gets.strip
+    puts "Please enter the first major of the student to be added."
+    add_major1 = gets.strip
+    puts "Please enter the second major of the student to be added. If none, hit enter."
+    add_major2 = gets.strip
+    if add_major2 == ""
+      add_major2 = nil
     end
-    puts "Student added!"
-    q_modify(csv)
-  else
-    puts "A required field was left blank! Ensure only specified fields are left blank."
-    add_csv(csv)
+    puts "Please enter the first minor of the student to be added. If none, hit enter."
+    add_minor1 = gets.strip
+    if add_minor1 == ""
+      add_minor1 = nil
+    end
+    puts "Please enter the second minor of the student to be added. If none, hit enter."
+    m4 = true
+    add_minor2 = gets.strip
+    if add_minor2 == ""
+      add_minor2 = nil
+    end
+
+    # Checks that required fields have been entered and adds student to section hash array
+    unless add_fname == "" or add_lname == "" or add_email == "" or add_section == "" or add_major1 == ""
+      student = Student.new(fname = add_fname, lname = add_lname, email = add_email, section = add_section, major1 = add_major1, major2 = add_major2, minor1 = add_minor1, minor2 = add_minor2)
+      if csv.has_key?(student.section)
+        array = csv[student.section]
+        array.push(student)
+        csv[student.section] = array
+      else
+        array = [student]
+        csv[student.section] = array
+      end
+      puts "Student added!"
+      return csv
+    else
+      puts "A required field was left blank! Ensure only specified fields are left blank."
+    end
   end
 end
 
@@ -111,19 +113,22 @@ def del_csv(csv)
     end
   end
 
-  puts "Are you sure you would like to remove this student? (y/n)"
-  del_stu = gets.strip
-  if del_stu == "Y" or del_stu == "y"
-    arr.delete_at(index)
-    csv[stu_section] = arr
-    puts "Student was removed."
-    q_modify(csv)
-  elsif del_stu == "N" or del_stu == "n"
-    puts "Student was not removed."
-    q_modify(csv)
-  else
-    puts "Not a valid response! Please try again."
-    q_modify(csv)
+  loop do
+    puts "Student found. Student information is displayed below."
+    puts "first_name: #{arr[index].fname}, last_name: #{arr[index].lname}, email: #{arr[index].email}, section: #{arr[index].section}, major1: #{arr[index].major1}, major2: #{arr[index].major2}, minor1: #{arr[index].minor1}, minor2: #{arr[index].minor2}"
+    puts "Are you sure you would like to remove this student? (y/n)"
+    del_stu = gets.strip
+    if del_stu == "Y" or del_stu == "y"
+      arr.delete_at(index)
+      csv[stu_section] = arr
+      puts "Student was removed."
+      return csv
+    elsif del_stu == "N" or del_stu == "n"
+      puts "Student was not removed."
+      return csv
+    else
+      puts "Not a valid response! Please try again."
+    end
   end
 end
 
@@ -160,78 +165,79 @@ def edit_csv(csv)
     end
   end
   puts "Student email found. Current data displayed below."
-  puts "first_name: #{arr[index].fname}, last_name: #{arr[index].lname}, email: #{arr[index].email}, section: #{arr[index].section}, major1: #{arr[index].major1}, major2: #{arr[index].major2}, minor1: #{arr[index].minor1}, minor2: #{arr[index].minor2}"
-  puts "Which of the above fields would you like to modify? Enter 'none' to modify nothing."
-  field = gets.strip
-  case field
-  when "first_name"
-    puts "Enter new student first name."
-    edit_fname = gets.strip
-    arr[index].fname = edit_fname
-    csv[stu_section] = arr
-    puts "Student modified!"
-    q_modify(csv)
-  when "last_name"
-    puts "Enter new student last name."
-    edit_lname = gets.strip
-    arr[index].lname = edit_lname
-    csv[stu_section] = arr
-    puts "Student modified!"
-    q_modify(csv)
-  when "email"
-    puts "Enter new student email."
-    edit_email = gets.strip
-    arr[index].email = edit_email
-    csv[stu_section] = arr
-    puts "Student modified!"
-    q_modify(csv)
-  when "section"
-    puts "Enter new student section."
-    edit_section = gets.strip
-    arr[index].section = edit_section
-    if csv.has_key?(edit_section)
-      array = csv[edit_section]
-      array.push(arr[index])
-      csv[edit_section] = array
+  loop do
+    puts "first_name: #{arr[index].fname}, last_name: #{arr[index].lname}, email: #{arr[index].email}, section: #{arr[index].section}, major1: #{arr[index].major1}, major2: #{arr[index].major2}, minor1: #{arr[index].minor1}, minor2: #{arr[index].minor2}"
+    puts "Which of the above fields would you like to modify? Enter 'none' to modify nothing."
+    field = gets.strip
+    case field
+    when "first_name"
+      puts "Enter new student first name."
+      edit_fname = gets.strip
+      arr[index].fname = edit_fname
+      csv[stu_section] = arr
+      puts "Student modified!"
+      return csv
+    when "last_name"
+      puts "Enter new student last name."
+      edit_lname = gets.strip
+      arr[index].lname = edit_lname
+      csv[stu_section] = arr
+      puts "Student modified!"
+      return csv
+    when "email"
+      puts "Enter new student email."
+      edit_email = gets.strip
+      arr[index].email = edit_email
+      csv[stu_section] = arr
+      puts "Student modified!"
+      return csv
+    when "section"
+      puts "Enter new student section."
+      edit_section = gets.strip
+      arr[index].section = edit_section
+      if csv.has_key?(edit_section)
+        array = csv[edit_section]
+        array.push(arr[index])
+        csv[edit_section] = array
+      else
+        csv[edit_section] = arr[index]
+      end
+      arr.delete_at(index)
+      puts "Student modified!"
+      return csv
+    when "major1"
+      puts "Enter new student 1st major."
+      edit_major1 = gets.strip
+      arr[index].major1 = edit_major1
+      csv[stu_section] = arr
+      puts "Student modified!"
+      return csv
+    when "major2"
+      puts "Enter new student 2nd major."
+      edit_major2 = gets.strip
+      arr[index].major2 = edit_major2
+      csv[stu_section] = arr
+      puts "Student modified!"
+      return csv
+    when "minor1"
+      puts "Enter new student 1st minor."
+      edit_minor1 = gets.strip
+      csv[stu_section] = arr
+      arr[index].minor1 = edit_minor1
+      puts "Student modified!"
+      return csv
+    when "minor2"
+      puts "Enter new student 2nd minor."
+      edit_minor2 = gets.strip
+      arr[index].minor2 = edit_minor2
+      csv[stu_section] = arr
+      puts "Student modified!"
+      return csv
+    when "none"
+      "Changed your mind? No problem!"
+      return csv
     else
-      csv[edit_section] = arr[index]
+      puts "Not a valid response. Please try again."
     end
-    arr.delete_at(index)
-    puts "Student modified!"
-    q_modify(csv)
-  when "major1"
-    puts "Enter new student 1st major."
-    edit_major1 = gets.strip
-    arr[index].major1 = edit_major1
-    csv[stu_section] = arr
-    puts "Student modified!"
-    q_modify(csv)
-  when "major2"
-    puts "Enter new student 2nd major."
-    edit_major2 = gets.strip
-    arr[index].major2 = edit_major2
-    csv[stu_section] = arr
-    puts "Student modified!"
-    q_modify(csv)
-  when "minor1"
-    puts "Enter new student 1st minor."
-    edit_minor1 = gets.strip
-    csv[stu_section] = arr
-    arr[index].minor1 = edit_minor1
-    puts "Student modified!"
-    q_modify(csv)
-  when "minor2"
-    puts "Enter new student 2nd minor."
-    edit_minor2 = gets.strip
-    arr[index].minor2 = edit_minor2
-    csv[stu_section] = arr
-    puts "Student modified!"
-    q_modify(csv)
-  when "none"
-    "Changed your mind? No problem!"
-    q_modify(csv)
-  else
-    puts "Not a valid response. Please try again."
-    edit_csv(csv)
   end
 end
--- src/file_handler.rb
+++ src/file_handler.rb
@@ -8,38 +8,42 @@ require 'csv'
 
 # Gets the input csv file
 def read_file
-  # Gets the file from the user
-  puts "Enter csv file name to be read from 'data' folder:"
-  file = gets.strip()
+  loop do
+    # Gets the file from the user
+    puts "Enter csv file name to be read from 'data' folder:"
+    file = gets.strip()
 
-  # Builds out filepath
-  filepath = File.expand_path("..", Dir.pwd)
-  filepath += "/data/#{file}"
+    # Builds out filepath
+    filepath = File.expand_path("..", Dir.pwd)
+    filepath += "/data/#{file}"
 
-  # Error handling for input file
-  valid_file = File.file?(filepath)
-  valid_csv = filepath.include? ".csv"
+    # Error handling for input file
+    valid_file = File.file?(filepath)
+    valid_csv = filepath.include? ".csv"
 
-  # Create CSV Table from file
-  if file_check(valid_file, valid_csv)
-    hash_creator(filepath)
-    puts "Student hash has been created."
-    return hash_creator(filepath)
+    # Create CSV Table from file
+    if file_check(valid_file, valid_csv)
+      hash_creator(filepath)
+      puts "Student hash has been created."
+      return hash_creator(filepath)
+    end
   end
 end
 
 # Checks whether or not a file is valid and a csv file
 def file_check(valid_file, valid_csv)
+  # Checks if a valid file was passed
   if valid_file
+    # Checks if valid file was a csv
     if valid_csv
       return true
     else
-      puts "Not a csv file. Please try again!"
-      read_file
+      puts "Not a csv file. Please try again."
+      return false
     end
   else
-    puts "Invalid file. Please try again!"
-    read_file
+    puts "Invalid file. Please try again."
+    return false
   end
 end
 
